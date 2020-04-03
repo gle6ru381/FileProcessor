2d6c2cd9858a6e78d671c2ed9693e0124f6ab9d7
Gleb <trogaev@bk.ru>
2020-04-03 22:53:52 +0700

    add main lainEdit and layout

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,27 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include <QGridLayout>
+#include <QLabel>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent)
 {
-    ui->setupUi(this);
+    QGridLayout* layout = new QGridLayout(this);
+    mask = new QLineEdit(this);
+    QLabel* labMask = new QLabel("Маска имени", this);
+    find = new QLineEdit(this);
+    replace = new QLineEdit(this);
+    QLabel* labFind = new QLabel("Найти", this);
+    QLabel* labReplace = new QLabel("Заменить", this);
+    layout->addWidget(mask, 0, 0);
+    layout->addWidget(labMask, 1, 0);
+    layout->addWidget(find, 0, 1);
+    layout->addWidget(replace, 0, 2);
+    layout->addWidget(labFind, 1, 1);
+    layout->addWidget(labReplace, 1, 2);
+    QWidget* centralWidget = new QWidget(this);
+    centralWidget->setLayout(layout);
+    setCentralWidget(centralWidget);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
 }
-
--- mainwindow.h
+++ mainwindow.h
@@ -1,21 +1,28 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QLineEdit>
 #include <QMainWindow>
+#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
+    QLineEdit* mask;
+    QLineEdit* find;
+    QLineEdit* replace;
+    QPushButton* fandr;
+    QPushButton* rollback;
 };
 #endif // MAINWINDOW_H
