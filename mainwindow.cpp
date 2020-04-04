687f01bbc5825578044acaa8320c95531f08598e
Gleb <trogaev@bk.ru>
2020-04-04 23:12:43 +0700

    add button 'find and replace' and 'rollback'

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,12 +11,17 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent)
     replace = new QLineEdit(this);
     QLabel* labFind = new QLabel("Найти", this);
     QLabel* labReplace = new QLabel("Заменить", this);
+    fandr = new QPushButton("Найти и заменить", this);
+    rollback = new QPushButton("Откат", this);
     layout->addWidget(mask, 0, 0);
-    layout->addWidget(labMask, 1, 0);
+    layout->addWidget(labMask, 1, 0, Qt::AlignTop);
     layout->addWidget(find, 0, 1);
     layout->addWidget(replace, 0, 2);
-    layout->addWidget(labFind, 1, 1);
-    layout->addWidget(labReplace, 1, 2);
+    layout->addWidget(labFind, 1, 1, Qt::AlignTop);
+    layout->addWidget(labReplace, 1, 2, Qt::AlignTop);
+    layout->addWidget(fandr, 2, 1);
+    layout->addWidget(rollback, 2, 2);
+
     QWidget* centralWidget = new QWidget(this);
     centralWidget->setLayout(layout);
     setCentralWidget(centralWidget);
