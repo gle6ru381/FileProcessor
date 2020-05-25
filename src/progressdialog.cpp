3f8d74f85ff6b8c77c9797a5921c1bf8d3f09247
Gleb <56115995+gle6ru381@users.noreply.github.com>
2020-05-25 19:01:14 +0700

    add construct for ProgressDialog

--- src/progressdialog.cpp
+++ src/progressdialog.cpp
@@ -1,6 +1,25 @@
 #include "progressdialog.h"
 
-ProgressDialog::ProgressDialog()
+ProgressDialog::ProgressDialog(QWidget* parent) : QDialog(parent)
 {
+}
 
+ProgressDialog::ProgressDialog(Qt::WindowFlags flags, QWidget* parent)
+    : QDialog(parent)
+{
+    setWindowFlags(flags);
+}
+
+ProgressDialog::ProgressDialog(QString const& title, QWidget* parent)
+    : QDialog(parent)
+{
+    setWindowTitle(title);
+}
+
+ProgressDialog::ProgressDialog(
+        QString const& title, Qt::WindowFlags flags, QWidget* parent)
+    : QDialog(parent)
+{
+    setWindowTitle(title);
+    setWindowFlags(flags);
 }
--- src/progressdialog.h
+++ src/progressdialog.h
@@ -7,6 +7,12 @@ class ProgressDialog : public QDialog {
     Q_OBJECT
 public:
     ProgressDialog(QWidget* parent = nullptr);
+    ProgressDialog(Qt::WindowFlags flags, QWidget* parent = nullptr);
+    ProgressDialog(QString const& title, QWidget* parent = nullptr);
+    ProgressDialog(
+            QString const& title,
+            Qt::WindowFlags flags,
+            QWidget* parent = nullptr);
 };
 
 #endif // PROGRESSDIALOG_H
